9f79dc666c2ca70eb4806e7fc06102e5b18e49c4
MungC2010G2 <zhaiduong26z@gmail.com>
2021-02-25 01:22:59 +0700

    load danh sách sản phẩm lên

---
+++ src/main/java/controller/ProductServlet.java
@@ -0,0 +1,46 @@
+package controller;
+
+import model.Product;
+import service.ProductService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "ProductServlet", urlPatterns = "/product")
+public class ProductServlet extends HttpServlet {
+    ProductService productService = new ProductService();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if(action == null){
+            action = "";
+        }
+        switch (action){
+            case "":
+                showAllProduct(request,response);
+                break;
+        }
+    }
+
+    private void showAllProduct(HttpServletRequest request, HttpServletResponse response) {
+        List<Product> products = productService.fillAll();
+        request.setAttribute("products",products);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("ManagerProduct.jsp");
+        try {
+            dispatcher.forward(request,response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

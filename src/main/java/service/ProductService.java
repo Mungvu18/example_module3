6c2e2a1b15edeb840a65857d3f003f50ae093edd
MungC2010G2 <zhaiduong26z@gmail.com>
2021-02-25 00:31:27 +0700

    hoàn thành product service

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -79,16 +79,64 @@ public class ProductService implements IProductService{
 
     @Override
     public boolean delete(int id) {
-        return false;
+        Boolean test = false;
+        Connection connection = ConnectionJDBC.getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(" delete from product where id = ?");
+            preparedStatement.setInt(1,id);
+            test = preparedStatement.executeUpdate() > 0;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return test;
     }
 
     @Override
     public Product findById(int id) {
-        return null;
+        Product product = null;
+        Connection connection = ConnectionJDBC.getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from  product where id = ?");
+            preparedStatement.setInt(1,id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                String name = resultSet.getString(2);
+                Double price = resultSet.getDouble(3);
+                int amount = resultSet.getInt(4);
+                String color = resultSet.getString(5);
+                String description = resultSet.getString(6);
+                String category_name = resultSet.getString(7);
+                product = new Product(id,name,price,amount,color,description,category_name);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return product;
     }
 
     @Override
     public List<Product> findByName(String name) {
-        return null;
+        List<Product> products = new ArrayList<>();
+        Connection connection = ConnectionJDBC.getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from  product where name like ?");
+            preparedStatement.setString(1,"%" +name+ "%");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                int id = resultSet.getInt(1);
+                Double price = resultSet.getDouble(3);
+                int amount = resultSet.getInt(4);
+                String color = resultSet.getString(5);
+                String description = resultSet.getString(6);
+                String category_name = resultSet.getString(7);
+                Product product = new Product(id,name,price,amount,color,description,category_name);
+                products.add(product);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return products;
     }
 }
